890105abcfeaa0f763b6ee23006b642e7d4cf3d1
Hlecler <Hugo.lecler@laposte.net>
2017-05-26 14:28:31 +0200

    Update header.php

--- header.php
+++ header.php
@@ -1,3 +1,3 @@
 <a href="index.php">
-<img src="images/logo.png" alt="Logo CodeAnon" style="width:354px;height:212px;" align="middle">
+<img src="images/logo.png" alt="Logo CodeAnon" style="width:354px;height:212px;vertical-align:middle">
 </a>
