db933d786de703fc90a8d9ab75f45d42ca1d2c67
Hlecler <Hugo.lecler@laposte.net>
2017-05-26 18:05:19 +0200

    Update PDO.php

--- model/PDO.php
+++ model/PDO.php
@@ -3,7 +3,7 @@ function connect()
 {
 	try
 	{
-	    $bd = new PDO('pgsql:host=ec2-54-246-108-119.eu-west-1.compute.amazonaws.com;port=5432;dbname=d8mv81rvehr8or', 'wftpzmckvuzkzd','62630f127cd96b941e7d16720368016da8382ba48183c9f5c91825098e2914ea');
+	    $bd = new PDO('pgsql:host=ec2-54-246-108-119.eu-west-1.compute.amazonaws.com;dbname=d8mv81rvehr8or', 'wftpzmckvuzkzd','62630f127cd96b941e7d16720368016da8382ba48183c9f5c91825098e2914ea');
 	}
 	catch (Exception $e)
 	{
