06ec2594c5407904a5f9dd506b10b25b99fff6ff
Hlecler <Hugo.lecler@laposte.net>
2017-05-26 21:22:22 +0200

    Update section.php

--- section.php
+++ section.php
@@ -1,6 +1,13 @@
 <!DOCTYPE html>
-<html>
-  <head>
+<html lang="fr" ng-app="CodeAnon">
+<head>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="Description" content="Imageboard based website created for a web architecture project by LECLER Hugo">
+<link rel="stylesheet" type="text/css" href="css/styles.css">
+<title>CodeAnon - Section</title>
+
 		<?php include("header.php")
 			if(isset($_GET['idsection']))
 			{
@@ -15,10 +22,24 @@
  
   
   <body>
-    <?php
+<?php
+ 	include("headersection.php");
+	include("header.php");
     while($donnees = $reponse->fetch())
     {
     echo $donnees['Title'].':'.$donnees['Content'].'\n';
     }
     ?>
-  </body>
+</body>
+
+<body>
+
+
+<!-- HTML CODE BELOW -->
+
+<!-- END HTML CODE -->
+<?php 
+  include("footer.php");
+?>
+</body>
+
