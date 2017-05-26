a585113d743d29e9bb4f59eae6b59354806dcd3d
Hlecler <Hugo.lecler@laposte.net>
2017-05-26 16:59:51 +0200

    Update PDO.php

--- model/PDO.php
+++ model/PDO.php
@@ -3,11 +3,10 @@ function connect()
 {
 	try
 	{
-	    $bd = new PDO('pgsql:host=ec2-54-246-108-119.eu-west-1.compute.amazonaws.com;port=5432;dbname=d8mv81rvehr8or;CHARSET=utf8', 'wftpzmckvuzkzd','62630f127cd96b941e7d16720368016da8382ba48183c9f5c91825098e2914ea');
+	    $bd = new PDO('pgsql:host=ec2-54-246-108-119.eu-west-1.compute.amazonaws.com;port=5432;dbname=d8mv81rvehr8or', 'wftpzmckvuzkzd','62630f127cd96b941e7d16720368016da8382ba48183c9f5c91825098e2914ea');
 	}
 	catch (Exception $e)
 	{
-			echo 'test';
 			die('Erreur : ' . $e->getMessage());
 	}
 	return($bd);
