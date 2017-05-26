b0e92ae3c4ee5623e728a73d22b3c62db638fcc0
Hlecler <Hugo.lecler@laposte.net>
2017-05-26 17:27:11 +0200

    Update section.php

--- section.php
+++ section.php
@@ -6,8 +6,7 @@ try
 
     // On se connecte à MySQL
 
-    $bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
-
+    $bdd = new PDO('pgsql:host=ec2-54-246-108-119.eu-west-1.compute.amazonaws.com;port=5432;dbname=d8mv81rvehr8or', 'wftpzmckvuzkzd','62630f127cd96b941e7d16720368016da8382ba48183c9f5c91825098e2914ea');
 }
 
 catch(Exception $e)
