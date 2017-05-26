c62ea38b7d8de5f89608d240bdac7ee962350d66
Hlecler <Hugo.lecler@laposte.net>
2017-05-26 18:56:51 +0200

    Update section.php

--- section.php
+++ section.php
@@ -2,7 +2,9 @@
 	echo "<ul class=\"section\">";
 	try
 	{
+		echo "bon"
 	    $bdSection = new PDO('pgsql:host=ec2-54-246-108-119.eu-west-1.compute.amazonaws.com;dbname=d8mv81rvehr8or', 'wftpzmckvuzkzd','62630f127cd96b941e7d16720368016da8382ba48183c9f5c91825098e2914ea', array(PDO::ATTR_ERRMODE => PDO::	ERRMODE_EXCEPTION));
+		echo "jour"
 	}
 	catch (Exception $e)
 	{
