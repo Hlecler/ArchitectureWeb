9f176a63ba2c71c9b1be8b1a28758ce583d455f0
Hlecler <Hugo.lecler@laposte.net>
2017-05-26 19:19:00 +0200

    Update section.php

--- section.php
+++ section.php
@@ -1,20 +1,20 @@
-
-						<?php
-							try
-							{
-								$bdd = new PDO('pgsql:host=ec2-54-247-166-129.eu-west-1.compute.amazonaws.com;dbname=d8d5o1h0870hg', 'tkiiknfwbenkoe', '52a255680d0d2409a7ccefbd3da3cba1740f34c9e7e879d3d331faad1796ab02', array(PDO::ATTR_ERRMODE => PDO::	ERRMODE_EXCEPTION)); // Connexion à la bdd
-							}
-							catch (Exception $e)
-							{
-			        			die('Erreur : ' . $e->getMessage());
-							}
-		
-							$reponse = $bdd->query('SELECT nomconst, idconst FROM CONSTELLATION WHERE hemisnord=b\'1\'');
-							while($donnees = $reponse->fetch())
-							{
-						?>
-								<li><a href=<?php echo 'constellation.php'.'?'.'id='.$donnees['idconst']; ?>><?php echo $donnees['nomconst']; ?></a></li>
-						<?php
-							}
-							$reponse->closeCursor();
-						?>
+<?php
+	echo "<ul type=\"section\">";
+	try
+	{
+		echo "bon"
+	    	$bdd = new PDO('pgsql:host=ec2-54-247-166-129.eu-west-1.compute.amazonaws.com;dbname=d8d5o1h0870hg', 'tkiiknfwbenkoe', '52a255680d0d2409a7ccefbd3da3cba1740f34c9e7e879d3d331faad1796ab02', array(PDO::ATTR_ERRMODE => PDO::	ERRMODE_EXCEPTION));
+		echo "jour"
+	}
+	catch (Exception $e)
+	{
+			die('Erreur : ' . $e->getMessage());
+	}
+	$reponse = $bdd->query('SELECT * FROM CONSTELLATION');
+	while($donnees = $reponse->fetch());
+	{
+		?><li><a href=<?php echo 'constellation.php'.'?'.'id='.$donnees['idconst']; ?>><?php echo $donnees['nomconst']; ?></a></li>
+<?php	}
+	$reponse->closeCursor();
+	echo "</ul>";
+?>
