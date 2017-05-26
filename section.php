1b09ab45e3da0482e261f5aa7fc5826728e4cec9
Hlecler <Hugo.lecler@laposte.net>
2017-05-26 15:12:31 +0200

    Update section.php

--- section.php
+++ section.php
@@ -1,24 +1,15 @@
 <?php 
 <?php
-  require_once 'model/PDO.php';
-  $bdSection = connect();
-  $listsection = $bdd->query('SELECT sectionname from section');
-
-affichersection($listsection);
-
-echo '<br />';
-
-//Permet d'afficher chaque section existante dans une liste html
-function affichersection($listsection)
-{
 	echo "<ul class=\"section\">";
-	foreach($listsection as $element)
+	require_once 'model/PDO.php';
+	$bdSection = connect();
+	$reponse = $bdd->query('SELECT * FROM section');
+	while($donnees = $reponse -> fetch());
 	{
-		echo "<li><a href=\"$element.php\">$element</a></li>";
+	echo "<li><a href=<?php echo "$donnees['sectionname'].php?id=".$donnees['idsection']; </a></li>";
 	}
-	echo "</ul>";
-}
-
-$listsection->closeCursor(); // Termine le traitement de la requête
-
+	$reponse->closeCursor();
+	?>
+	echo '</ul>';
+echo '<br />';
 ?>
