a8eca3e83467c14d5e5815b0cc1662e0fd076643
Hlecler <Hugo.lecler@laposte.net>
2017-05-26 17:26:25 +0200

    Update section.php

--- section.php
+++ section.php
@@ -1,21 +1,42 @@
-					<ul type="disk">
-						<?php
-							try
-							{
-								$bd = new PDO('pgsql:host=ec2-54-246-108-119.eu-west-1.compute.amazonaws.com;dbname=d8mv81rvehr8or', 'wftpzmckvuzkzd','62630f127cd96b941e7d16720368016da8382ba48183c9f5c91825098e2914ea');
-							}
-							catch (Exception $e)
-							{
-			        			die('Erreur : ' . $e->getMessage());
-							}
-		
-							$reponse = $bdd->query('SELECT Sectionname FROM Section');
-							while($donnees = $reponse->fetch())
-							{
-						?>
-								echo "<li><a href=\"$donnees['Sectionname'].php\"> $donnees['Sectionname']</a></li>";
-						<?php
-							}
-							$reponse->closeCursor();
-						?>
-					</ul>
+<?php
+
+try
+
+{
+
+    // On se connecte à MySQL
+
+    $bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
+
+}
+
+catch(Exception $e)
+
+{
+
+    // En cas d'erreur, on affiche un message et on arrête tout
+
+        die('Erreur : '.$e->getMessage());
+
+}
+
+
+$reponse = $bdd->query('SELECT * FROM Section');
+
+while ($donnees = $reponse->fetch())
+{
+?>
+    <p>
+
+    <?php echo $donnees['Sectionname']; ?>
+   </p>
+
+<?php
+
+}
+
+
+$reponse->closeCursor(); // Termine le traitement de la requête
+
+
+?>
