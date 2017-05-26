0656701da4062eb5f70ed811204cba8df55349a7
Hlecler <Hugo.lecler@laposte.net>
2017-05-26 19:23:28 +0200

    Update section.php

--- section.php
+++ section.php
@@ -1,20 +1,20 @@
 <?php
-	echo "<ul type=\"section\">";
+	echo "<ul class=\"section\">";
 	try
 	{
-		echo "bon";
-	    	$bdd = new PDO('pgsql:host=ec2-54-247-166-129.eu-west-1.compute.amazonaws.com;dbname=d8d5o1h0870hg', 'tkiiknfwbenkoe', '52a255680d0d2409a7ccefbd3da3cba1740f34c9e7e879d3d331faad1796ab02', array(PDO::ATTR_ERRMODE => PDO::	ERRMODE_EXCEPTION));
-		echo "jour";
+	    $bdSection = new PDO('pgsql:host=ec2-54-246-108-119.eu-west-1.compute.amazonaws.com;dbname=d8mv81rvehr8or', 'wftpzmckvuzkzd','62630f127cd96b941e7d16720368016da8382ba48183c9f5c91825098e2914ea', array(PDO::ATTR_ERRMODE => PDO::	ERRMODE_EXCEPTION));
 	}
 	catch (Exception $e)
 	{
+			echo 'bonjour';
 			die('Erreur : ' . $e->getMessage());
 	}
-	$reponse = $bdd->query('SELECT * FROM CONSTELLATION');
-	while($donnees = $reponse->fetch())
+	$reponse = $bdSection->query('SELECT * FROM section');
+	while($donnees = $reponse -> fetch())
 	{
-		?><li><a href=<?php echo 'constellation.php'.'?'.'id='.$donnees['idconst']; ?>><?php echo $donnees['nomconst']; ?></a></li>
+		?><li><a href=<?php echo $donnees['sectionname'].'.php'.'?'.'id='.$donnees['sectionname']; ?>><?php echo $donnees['sectionname']; ?></a></li>
 <?php	}
 	$reponse->closeCursor();
+	?>
 	echo "</ul>";
 ?>
