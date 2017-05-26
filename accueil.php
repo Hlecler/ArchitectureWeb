98c8c424035931d085d125c7fa5f384d6f03461f
Hlecler <Hugo.lecler@laposte.net>
2017-05-26 19:00:22 +0200

    Update accueil.php

--- accueil.php
+++ accueil.php
@@ -30,30 +30,6 @@
 <p> CodeAnon est un forum permettant de communiquer simplement et anonymement avec d'autres utilisateurs. Pas besoin de s'inscrire, il vous suffit de cliquer sur l'une des sections ci-dessous, et de créer un sujet de discussion ou de répondre à un sujet déjà existant. Pensez à lire les <a href ="regles.html">règles</a> avant de procéder, et de lire la <a href ="FAQ.html">FAQ</a> pour plus d'informations.
 
 <h2> Liste des sections </h2>
-<?php
-	echo "<ul class=\"section\">";
-	try
-	{
-		echo "bon"
-	    $bdSection = new PDO('pgsql:host=ec2-54-246-108-119.eu-west-1.compute.amazonaws.com;dbname=d8mv81rvehr8or', 'wftpzmckvuzkzd','62630f127cd96b941e7d16720368016da8382ba48183c9f5c91825098e2914ea', array(PDO::ATTR_ERRMODE => PDO::	ERRMODE_EXCEPTION));
-		echo "jour"
-	}
-	catch (Exception $e)
-	{
-			echo 'bonjour';
-			die('Erreur : ' . $e->getMessage());
-	}
-	$reponse = $bdSection->query('SELECT * FROM section');
-	while($donnees = $reponse -> fetch());
-	{
-	echo "<li><a href=\"$donnees['sectionname'].php\">$donnees['sectionname']</a></li>";
-	}
-	$reponse->closeCursor();
-	?>
-	echo "</ul>";
-?>
-
-  
   
 <?php 
   include('section.php');
