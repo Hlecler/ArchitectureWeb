81151a01515e433861a07122b3f02f957b02c27b
Unknown <Hugo.lecler@laposte.net>
2017-05-26 12:29:27 +0200

    Auto stash before merge of "master" and "origin/master"

--- header.php
+++ header.php
@@ -1,1 +1,5 @@
+<<<<<<< Updated upstream
 <img src="images/logo.png" alt="Logo CodeAnon" style="width:354px;height:212px;">
+=======
+<img src="images/logo.png" alt="Logo CodeAnon" style="width:354px;height:212px;">
+>>>>>>> Stashed changes
