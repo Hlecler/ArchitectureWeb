2ced0c29d4949c1f104c493f873618f1df6e08d6
Hlecler <Hugo.lecler@laposte.net>
2017-05-26 17:03:52 +0200

    Update section.php

--- section.php
+++ section.php
@@ -1,15 +1,21 @@
-<?php
-	$bd = new PDO('pgsql:host=ec2-54-246-108-119.eu-west-1.compute.amazonaws.com;port=5432;dbname=d8mv81rvehr8or;CHARSET=utf8', 'wftpzmckvuzkzd','62630f127cd96b941e7d16720368016da8382ba48183c9f5c91825098e2914ea');
-	echo "<ul class=\"section\">";
-	require_once 'model/PDO.php';
-	$bdSection = connect();
-	$reponse = $bd->query('SELECT * FROM section');
-	while($donnees = $reponse -> fetch());
-	{
-	echo "<li><a href=\"$donnees['sectionname'].php\"> $donnees['sectionname']</a></li>";
-	}
-	$reponse->closeCursor();
-	?>
-	echo "</ul>";
-echo '<br/>';
-?>
+					<ul type="disk">
+						<?php
+							try
+							{
+								$bdd = new PDO('pgsql:host=ec2-54-246-108-119.eu-west-1.compute.amazonaws.com;port=5432;dbname=d8mv81rvehr8or', 'wftpzmckvuzkzd','62630f127cd96b941e7d16720368016da8382ba48183c9f5c91825098e2914ea'));
+							}
+							catch (Exception $e)
+							{
+			        			die('Erreur : ' . $e->getMessage());
+							}
+		
+							$reponse = $bdd->query('SELECT idsection, sectionname FROM Section');
+							while($donnees = $reponse->fetch())
+							{
+						?>
+								<li><a href=<?php echo 'General.php'.'?'.'id='.$donnees['idsection']; ?>><?php echo $donnees['sectionname']; ?></a></li>
+						<?php
+							}
+							$reponse->closeCursor();
+						?>
+					</ul>
