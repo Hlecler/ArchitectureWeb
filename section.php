dc52815a0c840ae46ad2fe09d5f6f624d540dcba
Hlecler <Hugo.lecler@laposte.net>
2017-05-26 18:09:24 +0200

    Update section.php

--- section.php
+++ section.php
@@ -1,7 +1,13 @@
 <?php
 	echo "<ul class=\"section\">";
-	require_once 'model/PDO.php';
-	$bdSection = connect();
+	try
+	{
+	    $bdSection = new PDO('pgsql:host=ec2-54-246-108-119.eu-west-1.compute.amazonaws.com;dbname=d8mv81rvehr8or', 'wftpzmckvuzkzd','62630f127cd96b941e7d16720368016da8382ba48183c9f5c91825098e2914ea');
+	}
+	catch (Exception $e)
+	{
+			die('Erreur : ' . $e->getMessage());
+	}
 	$reponse = $bdSection->query('SELECT * FROM Section');
 	while($donnees = $reponse -> fetch());
 	{
