65ca5cc646f76cccdcef006b1aef275c1d48c0d1
Hlecler <Hugo.lecler@laposte.net>
2017-05-26 22:59:26 +0200

    Update newtopic.php

--- newtopic.php
+++ newtopic.php
@@ -3,9 +3,6 @@
 		<link rel="stylesheet" type="text/css" href="css/styles.css">
 	</head>
 	<body>
-		<?php
-			include 'headersection.php';
-		?>
 		<!-- HTML CODE BELOW -->
 		<form id="newtopicform" action="r_newtopic.php", method="post">
 			<b>Sujet: </b><input type="text" name="subject"><br />
@@ -15,9 +12,5 @@
 			<br />
 			<input type="submit" value="Envoyer">
 		</form>
-		<!-- END HTML CODE -->
-		<?php
-			include 'footer.php';
-		?>
 	</body>
 </html>
