08e30dbf2a25d56f157014e676fb173e1618a6d9
Hlecler <Hugo.lecler@laposte.net>
2017-05-26 22:23:43 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 <br>
 <p>Site crée dans le cadre d'un projet d'architecture web.</p>
 <img>
-<p>© 2017 Copyright - LECLER Hugo</p><p>
+<p>© 2017 Copyright - LECLER Hugo</p>
 <a href="mailto:hugo.lecler@laposte.net"><p>Contactez moi	</p></a>
 <img src="images/polytech.jpg" alt="Logo Polytech Montpellier" style="width:200;height:104px;vertical-align:left">
 
