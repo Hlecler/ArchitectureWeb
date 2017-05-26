2eeb5558aa01a60681a01694bad986d2940d49f9
Hlecler <Hugo.lecler@laposte.net>
2017-05-26 18:00:07 +0200

    Update section.php

--- section.php
+++ section.php
@@ -1,31 +1,13 @@
- <?php
- 
- try
- 
- {
- 
-     // On se connecte à MySQL
-     $bdd = new PDO('pgsql:host=ec2-54-246-108-119.eu-west-1.compute.amazonaws.com;port=5432;dbname=d8mv81rvehr8or', 'wftpzmckvuzkzd','62630f127cd96b941e7d16720368016da8382ba48183c9f5c91825098e2914ea', array(PDO::ATTR_ERRMODE => PDO::	ERRMODE_EXCEPTION));
- }
- 
- catch(Exception $e)
- 
- {
- 
-     // En cas d'erreur, on affiche un message et on arrête tout
- 
-         die('Erreur : '.$e->getMessage());
- 
- }
- 
- 
- $reponse = $bdd->query('SELECT SectionName FROM Section');
- 
- while ($donnees = $reponse->fetch())
- {
- ?>
-     <p>
- 
-     <?php echo $donnees['Sectionname']; ?>
-    </p>
- 
+<?php
+	echo "<ul class=\"section\">";
+	require_once 'model/PDO.php';
+	$bdSection = connect();
+	$reponse = $bdSection->query('SELECT * FROM Section');
+	while($donnees = $reponse -> fetch());
+	{
+	echo "<li><a href=\"$donnees['Sectionname'].php\">$donnees['Sectionname']</a></li>";
+	}
+	$reponse->closeCursor();
+	?>
+	echo "</ul>";
+?>
