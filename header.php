d5ef3f83aee112fa03757d065378c7d0df7d1ad1
Hlecler <Hugo.lecler@laposte.net>
2017-05-26 13:09:35 +0200

    Update header.php

--- header.php
+++ header.php
@@ -1,5 +1,1 @@
-<<<<<<< Updated upstream
 <img src="images/logo.png" alt="Logo CodeAnon" style="width:354px;height:212px;">
-=======
-<img src="images/logo.png" alt="Logo CodeAnon" style="width:354px;height:212px;">
->>>>>>> Stashed changes
