916b5ebacb949a76d5bb2cf10dacbc0e4806bba2
Hlecler <Hugo.lecler@laposte.net>
2017-05-26 17:44:02 +0200

    Update section.php

--- section.php
+++ section.php
@@ -5,9 +5,7 @@
  {
  
      // On se connecte à MySQL
- 
--    $bdd = new PDO('pgsql:host=ec2-54-246-108-119.eu-west-1.compute.amazonaws.com;port=5432;dbname=d8mv81rvehr8or', 'wftpzmckvuzkzd','62630f127cd96b941e7d16720368016da8382ba48183c9f5c91825098e2914ea');
-+    $bdd = new PDO('pgsql:host=ec2-54-246-108-119.eu-west-1.compute.amazonaws.com;port=5432;dbname=d8mv81rvehr8or', 'wftpzmckvuzkzd','62630f127cd96b941e7d16720368016da8382ba48183c9f5c91825098e2914ea', array(PDO::ATTR_ERRMODE => PDO::	ERRMODE_EXCEPTION));
+     $bdd = new PDO('pgsql:host=ec2-54-246-108-119.eu-west-1.compute.amazonaws.com;port=5432;dbname=d8mv81rvehr8or', 'wftpzmckvuzkzd','62630f127cd96b941e7d16720368016da8382ba48183c9f5c91825098e2914ea', array(PDO::ATTR_ERRMODE => PDO::	ERRMODE_EXCEPTION));
  }
  
  catch(Exception $e)
@@ -21,7 +19,7 @@
  }
  
  
- $reponse = $bdd->query('SELECT * FROM Section');
+ $reponse = $bdd->query('SELECT SectionName FROM Section');
  
  while ($donnees = $reponse->fetch())
  {
