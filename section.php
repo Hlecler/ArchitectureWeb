f8cdd49bf8b4600bd4cf048408a3e9f73f2711fb
Hlecler <Hugo.lecler@laposte.net>
2017-05-26 16:56:09 +0200

    Update section.php

--- section.php
+++ section.php
@@ -1,10 +1,9 @@
 <?php
-	require_once('model/PDO.php');
-	$bdSection = connect();
+	$bd = new PDO('pgsql:host=ec2-54-246-108-119.eu-west-1.compute.amazonaws.com;port=5432;dbname=d8mv81rvehr8or;CHARSET=utf8', 'wftpzmckvuzkzd','62630f127cd96b941e7d16720368016da8382ba48183c9f5c91825098e2914ea');
 	echo "<ul class=\"section\">";
 	require_once 'model/PDO.php';
 	$bdSection = connect();
-	$reponse = $bdSection->query('SELECT * FROM section');
+	$reponse = $bd->query('SELECT * FROM section');
 	while($donnees = $reponse -> fetch());
 	{
 	echo "<li><a href=\"$donnees['sectionname'].php\"> $donnees['sectionname']</a></li>";
