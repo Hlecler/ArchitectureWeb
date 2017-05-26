90c19611a43cdbb487c32fb70191c2527b6a414a
Hlecler <Hugo.lecler@laposte.net>
2017-05-27 00:02:58 +0200

    Update newtopic.php

--- newtopic.php
+++ newtopic.php
@@ -1,9 +1,3 @@
-<html>
-	<head>
-		<link rel="stylesheet" type="text/css" href="css/styles.css">
-	</head>
-	<body>
-		<!-- HTML CODE BELOW -->
 		<form action="/r_newtopic.php" method="post">
 			<b>Sujet:<br> </b><input type="text" name="subject"><br />
 			<br/>
@@ -12,5 +6,4 @@
 			<br/>
 			<input type="submit" value="Envoyer">
 		</form>
-	</body>
-</html>
+
