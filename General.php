7d7bb1755db5036efb14e314f7d3ba532b762b7b
Hlecler <Hugo.lecler@laposte.net>
2017-05-26 13:30:25 +0200

    Update General.php

--- General.php
+++ General.php
@@ -6,19 +6,8 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="Description" content="Imageboard based website created for a web architecture project by LECLER Hugo">
 <link rel="stylesheet" type="text/css" href="css/styles.css">
-<title>CodeAnon - Accueil</title>
+<title>CodeAnon - General</title>
 
-<!-- Bootstrap CSS -->
-<link href="/lib/bootstrap-3.3.7/css/bootstrap.min.css" rel="stylesheet">
-<link href="/front/css/reset.css" rel="stylesheet">
-<link href="/front/css/globalStyle.css" rel="stylesheet">
-<link href="/front/components/base/base.css" rel="stylesheet">
-<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-<!--[if lt IE 9]>
-<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-<![endif]-->
 
 </head>
 
