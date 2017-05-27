7bcaf1ebeb59d8338a0ea987362bdf61dc3e4041
Hlecler <Hugo.lecler@laposte.net>
2017-05-27 15:33:01 +0200

    Update newpost.php

--- newpost.php
+++ newpost.php
@@ -2,6 +2,7 @@
 			<b>Answer?:<br></b>
 			<textarea name="message" form="newpostform">Your Message...</textarea>
 			<br/>
+			<input type='hidden' name="idthread" value="<?php echo $_GET['idthread']; ?> " />
 			<input type="submit" value="Envoyer">
 			<input type='hidden' name='idthread1' value='<?php echo $_GET['idthread'];?>'>
 		</form>
