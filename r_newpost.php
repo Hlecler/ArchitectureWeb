145fcf9d6c3a388a06d98ecdfc20f452f4615b53
Hlecler <Hugo.lecler@laposte.net>
2017-05-27 15:53:40 +0200

    Update r_newpost.php

--- r_newpost.php
+++ r_newpost.php
@@ -4,7 +4,8 @@
 	function redirect($idthread) {
 		header('Location: thread.php?idthread='.$idthread);
 	}
-	
+	$maxId = myPDO()->query('SELECT MAX(idpost) FROM post');
+	$maxId ++;
 	$message = $_POST['message'];
 	$idthread = $_POST['idthread'];
 	if(empty($message) or empty($idthread)) {
@@ -13,7 +14,7 @@
 	}
 	
 	$dbnewpost = connect();
-	$dbnewpost->exec("INSERT INTO post VALUES (1, $message,$idthread)");
+	$dbnewpost->exec("INSERT INTO post VALUES ($maxId, $message,$idthread)");
 	
 	redirect($idthread);
 ?>
