544b1744ff6c3610865a358213f1167992cb5d5c
Hlecler <Hugo.lecler@laposte.net>
2017-05-27 07:37:59 +0200

    Update newpost.php

--- newpost.php
+++ newpost.php
@@ -1,5 +1,5 @@
 		<form id="newpostform" action="/r_newpost.php" method="post">
-			<b>Message<br></b>
+			<b>Answer?:<br></b>
 			<textarea name="message" form="newpostform">Your Message...</textarea>
 			<br/>
 			<input type="submit" value="Envoyer">
