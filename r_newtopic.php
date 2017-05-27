b8dd0601eb260c69e87fe2f8bc79c2c843838cfb
Hlecler <Hugo.lecler@laposte.net>
2017-05-27 09:43:21 +0200

    Update r_newtopic.php

--- r_newtopic.php
+++ r_newtopic.php
@@ -1,6 +1,6 @@
 <?php
 	require_once 'model/PDO.php';
-	//require_once 'model/newthread.php';
+	require_once 'model/newthread.php';
 	function redirect($idsection) {
 		header('Location: section.php?idsection='.$idsection);
 		exit;
@@ -14,8 +14,8 @@
 		redirect($idsec);
 	}
 	
-	//newthread($subject, $message, $idsec);
-	$dbquery = connect();
-	$dbquery->exec('INSERT INTO thread(Title, Content, Idsection) VALUES ($subject, $message, 1)');
+	newthread($subject, $message, $idsec);
+	//$dbquery = connect();
+	//$dbquery->exec('INSERT INTO thread(Title, Content, Idsection) VALUES ($subject, $message, 1)');
 	redirect($idsec);
 ?>
