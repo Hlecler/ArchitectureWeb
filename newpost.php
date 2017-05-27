4aeca796bdd7b703f77d45c09dc3468626e67218
Hlecler <Hugo.lecler@laposte.net>
2017-05-27 07:25:31 +0200

    Update newpost.php

--- newpost.php
+++ newpost.php
@@ -1,22 +1,6 @@
-<html>
-	<head>
-		<link rel="stylesheet" type="text/css" href="css/styles.css">
-	</head>
-	<body>
-		<?php
-			include 'headersection.php';
-			include 'header.php';
-		?>
-		<!-- HTML CODE BELOW -->
-		<form id="newpostform" action="r_post.php", method="post">
-			<b>Message</b>
-			<textarea name="message" form="newpostform">Your Message...<textarea>
-			<br />
+		<form id="newpostform" action="/r_post.php" method="post">
+			<b>Message<br></b>
+			<textarea name="message" form="newpostform">Your Message...</textarea>
+			<br/>
 			<input type="submit" value="Envoyer">
 		</form>
-		<!-- END HTML CODE -->
-		<?php
-			include 'footer.php';
-		?>
-	</body>
-</html>
