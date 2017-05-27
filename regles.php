af8754465e128007967de4760f97c5188f1bfd3b
Hlecler <Hugo.lecler@laposte.net>
2017-05-27 08:10:18 +0200

    Update regles.php

--- regles.php
+++ regles.php
@@ -1,1 +1,22 @@
-Rien pour l'instant
+<!DOCTYPE html>
+<html lang="fr" ng-app="CodeAnon">
+<head>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="Description" content="Imageboard based website created for a web architecture project by LECLER Hugo">
+<link rel="stylesheet" type="text/css" href="css/styles.css">
+<title>CodeAnon - Règles</title>
+  
+</head>
+
+<body>
+<?php include'header.php';?>
+
+  <h4> Liste des règles: </h4>
+  <ul>
+    <li>Soyez respectueux.</li>
+    <li>Soyez responsable de ce que vous écrivez, même anonyme.</li>
+  </ul>
+<?php include'footer.php';?>
+</body>
