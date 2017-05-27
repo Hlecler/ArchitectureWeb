ce39ab0eeab20c41fe7e1ee0ae2a3f0a7c95d226
Hlecler <Hugo.lecler@laposte.net>
2017-05-27 15:38:36 +0200

    Update r_newpost.php

--- r_newpost.php
+++ r_newpost.php
@@ -1,18 +1,18 @@
 <?php
 	require_once 'model/PDO.php';
 	
-	function redirect() {
-		header('Location: section.php?idsection='.$idthread);
+	function redirect($idthread) {
+		header('Location: thread.php?idthread='.$idthread);
 	}
 	
 	$message = $_POST['message'];
 	$idthread = $_POST['idthread'];
 	if(empty($message) or empty($idthread)) {
-		redirect();
+		redirect($idthread);
 	}
 	
 	$dbnewpost = connect();
 	$dbnewpost->exec('INSERT INTO Post(Content,Idthread) VALUES ($message,$idthread)');
 	
-	redirect();
+	redirect($idthread);
 ?>
