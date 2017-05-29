0a3dd8c79a86ddaed7219cd57496ff08de3f8ada
Hlecler <Hugo.lecler@laposte.net>
2017-05-29 15:19:33 +0200

    Update r_newtopic.php

--- r_newtopic.php
+++ r_newtopic.php
@@ -1,6 +1,5 @@
 <?php
 	require_once 'model/PDO.php';
-	require_once 'model/newthread.php';
 	function redirect($idsection) {
 		header('Location: section.php?idsection='.$idsection);
 		exit;
@@ -10,17 +9,13 @@
 	$message = $_POST['message'];
 	$idsection = $_POST['idsection'];
 	
-	echo $idsection;
-	echo $subject;
-	echo $message;
 	$idmax = connect()->query('SELECT MAX(idsection) FROM thread');
         $idthread = $idmax->fetch()[0] +1;
-	echo $idthread;
+
 	if(empty($subject) or empty($message)) {
 		redirect($idsection);
 	}
 	
-	newthread($subject, $message, $idsection);
 	$dbquery = connect();
 	$dbquery->exec("INSERT INTO thread VALUES ($idthread, $subject, $message, $idsection)");
 	//redirect($idsection)
