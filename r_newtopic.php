b3d8e8837c49949bce33f1531ff653ec968040aa
Hlecler <Hugo.lecler@laposte.net>
2017-05-29 17:45:32 +0200

    Update r_newtopic.php

--- r_newtopic.php
+++ r_newtopic.php
@@ -22,7 +22,7 @@
 	//$test = "INSERT INTO Thread (idthread, idsection) VALUES (5,5)";
  	$test = "INSERT INTO Thread (idthread, idsection) VALUES(".$idthread.", ".$idsection");";
 
-	$test = "INSERT INTO Thread (idthread, title, content, idsection) VALUES (5,'ab','cd',5)";
+	//$test = "INSERT INTO Thread (idthread, title, content, idsection) VALUES (5,'ab','cd',5)";
         //$sql = "INSERT INTO Thread VALUES (:idthread, :idsection)";
 	$dbquery->exec($test);
 	redirect($idsection);
