68426b79170004b4eea1da310b0e085d5b3d98c9
Hlecler <Hugo.lecler@laposte.net>
2017-05-29 18:36:38 +0200

    Update r_newpost.php

--- r_newpost.php
+++ r_newpost.php
@@ -16,7 +16,7 @@
 	if(empty($message) or empty($idthread)) {
 		redirect($idthread);
 	}
-	echo "INSERT INTO Post (idpost, content, idthread) VALUES ("$idpost","$message","$idthread")";
+	echo "INSERT INTO Post (idpost, content, idthread) VALUES (".$idpost.",".$message.",".$idthread.")";
 	$dbnewpost = connect();
 	$dbnewpost->exec('INSERT INTO Post (idpost, content, idthread) VALUES ('.$idpost.','.$message.','.$idthread.')');
 	//redirect($idthread);
