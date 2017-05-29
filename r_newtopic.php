9c481d695744184ad79d9160e6a60f126441d8cc
Hlecler <Hugo.lecler@laposte.net>
2017-05-29 15:15:36 +0200

    Update r_newtopic.php

--- r_newtopic.php
+++ r_newtopic.php
@@ -14,14 +14,14 @@
 	echo $subject;
 	echo $message;
 	$idmax = connect()->query('SELECT MAX(idsection) FROM thread');
-        $idmax->fetch()[0];
-	echo $idmax;
+        $idthread = $idmax->fetch()[0];
+	echo $idthread;
 	if(empty($subject) or empty($message)) {
 		redirect($idsection);
 	}
 	
 	newthread($subject, $message, $idsection);
 	$dbquery = connect();
-	$dbquery->exec('INSERT INTO thread VALUES (1, $subject, $message, $idsection)');
-	//redirect($idsection);
+	$dbquery->exec("INSERT INTO thread VALUES (: ($idthread, $subject, $message, $idsection)");
+	//redirect($idsection)
 ?>
