205183eb0a2223182a8504c8ec6c166491bdff50
Hlecler <Hugo.lecler@laposte.net>
2017-05-29 19:26:19 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
 <img>
 <p>© 2017 Copyright - LECLER Hugo</p>
 <a href="mailto:hugo.lecler@laposte.net"><p>Contactez moi	</p></a>
-<img src="images/polytech.jpg" alt="Logo Polytech Montpellier" style="width:200;height:104px;vertical-align:left">
+<img src="images/polytech.jpg" alt="Logo Polytech Montpellier" align="right" style="width:200;height:104px;vertical-align:left">
 
-<img src="images/Picto_IG.jpg" alt="Logo Informatique et Gestion" style="width:68px;height:90px;vertical-align:right">
+<img src="images/Picto_IG.jpg" alt="Logo Informatique et Gestion" align="right" style="width:68px;height:90px;vertical-align:right">
 </footer>
