2027636bd9d44dbb3bf34a097e27d22dda10a472
Hlecler <Hugo.lecler@laposte.net>
2017-05-29 17:13:17 +0200

    Update r_newtopic.php

--- r_newtopic.php
+++ r_newtopic.php
@@ -20,7 +20,7 @@
 	//$dbquery->exec('UPDATE Thread SET idsection = 1 WHERE idsection = 3');
 	//$dbquery->exec('DELETE FROM Thread WHERE idthread = 3');
 	//$test = 'INSERT INTO Thread VALUES ($idthread, $subject, $message, $idsection)';
-	$test = 'INSERT INTO Thread (idthread) VALUES (4)';
+	$test = 'INSERT INTO Thread (idthread, title) VALUES (4, ab)';
 	$dbquery->exec($test);
 	redirect($idsection);
 ?>
