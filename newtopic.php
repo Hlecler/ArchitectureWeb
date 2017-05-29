0b16d8caf3769aee74e3a03e66e96aa0793c1b51
Hlecler <Hugo.lecler@laposte.net>
2017-05-29 19:22:36 +0200

    Update newtopic.php

--- newtopic.php
+++ newtopic.php
@@ -1,8 +1,8 @@
 		<form id="newtopicform" action="/r_newtopic.php" method="post">
-			<b>Sujet:<br> </b><input type="text" name="subject"><br />
+			<b>Sujet:<br> </b><input type="text" name="subject" placeholder="Enter a title"><br />
 			<br/>
 			<b>Message<br></b>
-			<textarea name="message" form="newtopicform">Your Message</textarea>
+			<textarea name="message" form="newtopicform" placeholder="Enter a message"></textarea>
 			<input type='hidden' name="idsection" value="<?php echo $_GET['idsection']; ?>" />
 			<br/>
 			<input type="submit" value="Envoyer">
