d4f178094a31b44cc869d1d14ddc70f2b1be4330
Hlecler <Hugo.lecler@laposte.net>
2017-05-29 15:24:55 +0200

    Update r_newpost.php

--- r_newpost.php
+++ r_newpost.php
@@ -1,27 +1,22 @@
 <?php
 	require_once 'model/PDO.php';
-	
-	function IdMax() {
-        	$maxId = connect()->query('SELECT MAX(idpost) FROM post');
-        	return $maxId->fetch()[0];
-    	}
 
 	function redirect($idthread) {
 		header('Location: thread.php?idthread='.$idthread);
+		exit;
 	}
 
 	$message = $_POST['message'];
 	$idthread = $_POST['idthread'];
+
+	$idmax = connect()->query('SELECT MAX(idpost) FROM post');
+        $idpost = $idmax->fetch()[0] +1;
+
 	if(empty($message) or empty($idthread)) {
 		redirect($idthread);
-
 	}
-	echo 'test1';
+
 	$dbnewpost = connect();
-	echo 'test2';
-	$Idpost =IdMax() + 1;
-	echo 'test3';
-	$dbnewpost->exec('INSERT INTO post VALUES ($Idpost, $message,$idthread)');
-	
+	$dbnewpost->exec('INSERT INTO post VALUES ($IdPost,$message,$idthread)');
 	redirect($idthread);
 ?>
