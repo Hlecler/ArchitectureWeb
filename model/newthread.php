932039e4c20d5fcccb6238178b0d099726865745
Hlecler <Hugo.lecler@laposte.net>
2017-05-29 14:42:00 +0200

    Update newthread.php

--- model/newthread.php
+++ model/newthread.php
@@ -8,3 +8,19 @@
 		$chg->bindParam(':idsection',$idsection);
 		$returnValue = $chg->execute();
 	}
+
+	function IdMax() {
+        	$idmax = connect()->query('SELECT MAX(idsection) FROM thread');
+        	return $idmax->fetch()[0];
+	}
+
+	public function newthread($Title, $Content, $idsection) {
+        $idthread = $this->IdMax() + 1;
+        $code = "INSERT INTO songs VALUES (:idthread, :title, :content, :idsection)";
+        $req = connect()->prepare($code);
+        $params = [
+          ':idthread' => $idthread,
+          ':title' => $Title,
+          ':content' => $Content,
+          ':idsection' => $idsection
+        ];
