a139cc56b7a97e3c661b79eeed370083aa8843e7
Hlecler <Hugo.lecler@laposte.net>
2017-05-29 19:29:11 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,10 +1,12 @@
 <footer class="footer">
 <br>
-<p>Site crée dans le cadre d'un projet d'architecture web.</p>
+<p>Site crée dans le cadre d'un projet d'architecture web.
+<img src="images/Picto_IG.jpg" alt="Logo Informatique et Gestion" align="right" style="float:right;width:68px;height:90px;vertical-align:left">
+<img src="images/polytech.jpg" alt="Logo Polytech Montpellier" align="right" style="float:right;width:200;height:104px;vertical-align:right">
+</p>
 <img>
 <p>© 2017 Copyright - LECLER Hugo</p>
 <a href="mailto:hugo.lecler@laposte.net"><p>Contactez moi	</p></a>
-<img src="images/polytech.jpg" alt="Logo Polytech Montpellier" align="right" style="width:200;height:104px;vertical-align:left">
 
-<img src="images/Picto_IG.jpg" alt="Logo Informatique et Gestion" align="right" style="width:68px;height:90px;vertical-align:right">
+
 </footer>
